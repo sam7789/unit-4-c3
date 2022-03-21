b1576fbd4dd632b9f0a34aae257e66fc8fd30d3d
Ayush Batra <ayushbatra128@gmail.com>
2022-03-21 11:50:12 +0530

    git gardian fix

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-let username = "sam7789";
-let password = "XN0ynbM9n8KmXqZL"
+// let username = "sam7789";
+// let password = "XN0ynbM9n8KmXqZL"
 
 const connect = ()=>{
     return mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.omfqq.mongodb.net/evaluation2?retryWrites=true&w=majority`);
