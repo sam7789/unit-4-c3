55911e3d47794555929773ba532893b6babe2d7c
Ayush Batra <ayushbatra128@gmail.com>
2022-03-21 11:34:31 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const userControllers = require('./controllers/user.controller');
 
 const app = express();
+app.use(express.json());
+
+app.use("users",userControllers)
 
 module.exports = app;
